f787be4077c672f76ea8edf5ed8d6f8aac04f77d
sundachase <46916447+sundachase@users.noreply.github.com>
2022-09-06 01:49:28 -0400

    Create Tweet.cs

---
+++ DataAPI/models/Tweet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static DataAPI.Controllers.TwitterController;
+
+namespace DataAPI.Models
+{
+	public class Tweet
+	{
+        public string? content { get; set; }
+        
+        public Twitter? user { get; set; }
+    }
+
+    public class Twitter
+    {
+        public string? username { get; set; }
+        public int followersCount { get; set; }
+        public int friendsCount { get; set; }
+    }
+}
+
